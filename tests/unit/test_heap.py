f260d5d30f0bbf9f947b0469bf19a3beddd8a454
Pebaz <pebazium@gmail.com>
2020-07-31 11:32:35 -0400

    added unit tests for test_heap

---
+++ tests/unit/test_heap.py
@@ -0,0 +1,26 @@
+from pbz.heap import *
+
+def test_creation():
+    h = MinHeap([4, 5, 2, 1, 9, 10])
+    assert h.pop() == 1
+    assert h.pop() == 2
+    assert h.pop() == 4
+
+    h = MaxHeap([4, 5, 2, 1, 9, 10])
+    assert h.pop() == 10
+    assert h.pop() == 9
+    assert h.pop() == 5
+
+def test___repr__():
+    pass
+
+def test_assignment():
+    pass
+
+def test___iter__():
+    pass
+
+
+
+def test_comparison_operators():
+    pass
