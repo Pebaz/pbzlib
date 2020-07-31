b3f2b78fbe483aee70b27a27ef7a14a7c7b4fe93
Pebaz <pebazium@gmail.com>
2020-07-31 10:53:00 -0400

    betterpyjson

---
+++ pbz/betterpyjson.py
@@ -0,0 +1,57 @@
+"""
+Interacting with JSON from Python should be as natural as it is in JavaScript.
+
+(JSON objects in Python should allow you to use the dot `.` operator to access
+fields.)
+"""
+
+import json
+from types import SimpleNamespace
+
+
+def load_dict(obj):
+	return load_str(json.dumps(obj))
+
+
+def load_str(text):
+	return json.loads(text, object_hook=lambda d: BetterPyJSON(**d))
+
+
+class BetterPyJSON(SimpleNamespace):
+	"""
+	"""
+
+	def __getitem__(self, key):
+		"""
+		Overload index operator.
+		Args:
+			key(object): Any valid Python object key.
+		
+		Returns:
+			The corresponding object contained within this namespace.
+		"""
+		return getattr(self, key)
+
+	def __len__(self):
+		return len(self.attributes())
+
+	def __setattr__(self, name, value):
+		if self.__dict__.get(name, False):
+			self.__dict__[name] = value
+
+	def __repr__(self):
+		return str(self.__dict__)
+
+	def __iter__(self):
+		return self.keys()
+
+	def keys(self):
+		for i in self.attributes():
+			yield i
+
+	def values(self):
+		for i in self.attributes():
+			yield self[i]
+
+	def attributes(self):
+		return list(set(dir(self)) - set(dir(BetterPyJSON)))
---
+++ tests/unit/test_betterpyjson.py
@@ -0,0 +1,25 @@
+import sys; sys.path.append('..')  # Allow tests to see betterpyjson package
+from pbz.betterpyjson import *
+
+
+def test_creation():
+	obj = load_dict(dict(name='Pebaz', age=24))
+	assert obj.name == 'Pebaz'
+	assert obj.age  == 24
+
+
+def test___repr__():
+	obj = load_dict(dict(name='Pebaz', age=24))
+	assert str(obj) == "{'name': 'Pebaz', 'age': 24}"
+
+
+def test_assignment():
+	obj = load_dict(dict(name='Pebaz', age=24))
+	obj.age = 32
+	assert obj.age == 32
+
+
+def test___iter__():
+	obj = load_dict(dict(name='Pebaz', age=24))
+	assert set(obj.keys())   == {'name', 'age'}
+	assert set(obj.values()) == {'Pebaz', 24}
