f975d99ceb3161979d33945adbb97c4659dcedce
Pebaz <pebazium@gmail.com>
2021-03-03 09:54:11 -0500

    dsa

---
+++ pbz/dsa/linked_lists.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+    def __str__(self):
+        return str(self.value)
+    def __repr__(self):
+        return str(self)
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.length = 2 if head and tail else (1 if head or tail else 0)
+        self.head = head
+        self.tail = tail
+    def extend(self, iterable):
+        for i in iterable:
+            self.insert(i)
+    def insert(self, node):
+        if self.head and self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+        self.length += 1
+    def search(self, value):
+        node = self.head
+        while node:
+            if node.value == value:
+                return node
+            else:
+                node = node.next
+    def delete(self, value):
+        removed = None
+        if self.length == 0:
+            return
+        elif self.length == 1:
+            if self.head.value == value:
+                removed = self.head
+                self.head = None
+                self.tail = None
+        elif self.length > 1:
+            if self.head.value == value:
+                removed = self.head
+                self.head = self.head.next
+                self.length -= 1
+                return removed
+            prev = self.head
+            node = self.head.next
+            while node:
+                if node.value == value:
+                    removed = node
+                    prev.next = node.next
+                    break
+                else:
+                    prev = node
+                    node = node.next
+            if removed == self.tail:
+                self.tail = prev
+        if removed:
+            self.length -= 1
+            return removed
+    def __str__(self):
+        l = []
+        node = self.head
+        while node:
+            l.append(node.value)
+            node = node.next
+        return str(l)
+    def __repr__(self):
+        return str(self)
