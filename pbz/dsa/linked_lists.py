3b6f31d3c128848585b353a094be28c8e5842c65
Pebaz <pebazium@gmail.com>
2021-03-08 09:23:24 -0500

    doubly linked list

--- pbz/dsa/linked_lists.py
+++ pbz/dsa/linked_lists.py
@@ -1,7 +1,8 @@
 class Node:
-    def __init__(self, value, next=None):
+    def __init__(self, value, next=None, prev=None):
         self.value = value
         self.next = next
+        self.prev = prev
 
     def __str__(self):
         return str(self.value)
@@ -11,8 +12,13 @@ class Node:
 
 
 class LinkedList:
+    """
+    Singly Linked List.
+
+    NOTE: Does not even touch Node.prev as that is DoublelyLinkedList's purpose.
+    """
     def __init__(self, head=None, tail=None):
-        self.length = sum((head, tail))
+        self.length = sum((bool(head), bool(tail)))
         self.head = head
         self.tail = tail
 
@@ -37,7 +43,7 @@ class LinkedList:
             else:
                 node = node.next
 
-    def delete(self, value):  # O(N) time | O(1) space
+    def remove(self, value):  # O(N) time | O(1) space
         removed = None
 
         if self.length == 0:
@@ -113,3 +119,84 @@ class LinkedList:
     
     def __len__(self):
         return self.length
+
+
+
+class DoublelyLinkedList:
+    def __init__(self, head=None, tail=None):
+        self.length = sum((bool(head), bool(tail)))
+        self.head = head
+        self.tail = tail
+    
+    def insert(self, node):
+        if self.tail:  # At least 1 node
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+
+        else:  # 0 nodes in list
+            self.head = node
+            self.tail = node
+
+        self.length += 1
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    
+    def __reversed__(self):
+        node = self.tail
+        while node:
+            yield node
+            node = node.prev
+
+    def search(self, value):
+        for node in self:
+            if node.value == value:
+                return node
+    
+    def extend(self, nodes):
+        for n in nodes:
+            self.insert(n)
+    
+    def __str__(self):
+        return str([n for n in self])
+
+    def __repr__(self):
+        return str(self)
+    
+    def __len__(self):
+        return self.length
+
+    def remove(self, value):
+        if not len(self):
+            return
+        
+        elif len(self) == 1:
+            if self.head.value == value:
+                tmp = self.head
+                self.head = None
+                self.tail = None
+                self.length -= 1
+                return tmp
+        
+        else:  # Find & remove node
+            node = self.head
+            while node:
+                print(node)
+                if node.value == value:
+                    node.prev.next = node.next
+
+                    if node is self.tail:
+                        self.tail = node.prev
+                    else:
+                        node.next.prev = node.prev
+
+                    self.length -= 1
+                    return node
+
+                node = node.next
+
+
